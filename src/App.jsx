893b70ff2c502ddc0aac2c1de4eb3a93a648604e
CloeDelage2301 <cloe.delage1@etu.unilim.fr>
2026-09-04 15:13:12 +0200

    Exo 1 fini

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,44 @@
-
-function User({src, name, work}) {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <p>Work : {work}</p>
-    </div>
-  );
-}
+import Profile from "./components/Profile";
 
 function App() {
-  
   return (
-    <>
-    <div>
-      <h1> Room </h1>
-      <h2> 5 people are around right now</h2>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md border-gray-200 bg-white p-6 shadow-lg">
+        <div className="mb-8 ">
+          <h1 className="text-3xl font-bold text-black">Room</h1>
+          <p className="text-sm font-medium text-gray-400">5 people are around right now</p>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <Profile
+            name="Dale Houston"
+            job="Software Engineer"
+            image="https://picsum.photos/200/300?1"
+          />
+          <Profile
+            name="Madge Murphy"
+            job="Product Designer"
+            image="https://picsum.photos/200/300?2"
+          />
+          <Profile
+            name="Margaret Garner"
+            job="Writer & Journalist"
+            image="https://picsum.photos/200/300?3"
+          />
+          <Profile
+            name="Gertrude Roberts"
+            job="Fashion Designer"
+            image="https://picsum.photos/200/300?4"
+          />
+          <Profile
+            name="Joshua Olson"
+            job="Photographer"
+            image="https://picsum.photos/200/300?5"
+          />
+        </div>
+      </div>
     </div>
- 
-      <User name="Dale Houston" work="Software Engineer" />
-      <User name="Madge Murphy" work="Product Designer" />
-      <User name="Margaret Garner" work="Writer & Journalist" />
-      <User name="Gertrude Roberts" work="Fashion Designer" />
-      <User name="Joshua Olson" work="Photographer" />
-  
-  </>
   );
 }
 
-export default App
+export default App;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,34 @@
-function Profile() {
+import Button from "./Button";
+
+function Profile({ name, job, image }) {
     return (
-        <>
-            < h1 className="profile">Bob's Todos</h1 >
-            <img src="https://i.imgur.com/yXOvdOSs.jpg" />
-            <ul>
-                <li> Invent new traffic lights</li>
-                <li> Rehearse a movie scene </li>
-                <li> Improve the spectrum technology</li>
-            </ul>
-        </>
+        <section className="flex items-center gap-4 border-2 border-gray-200 bg-gray-50 p-4 rounded-xl">
+            <div className="flex gap-4">
+                <img src={image} alt={name} className="h-12 w-12 rounded-full object-cover" />
+                <div className="min-w-0">
+                    <h2 className="font-bold text-black">{name}</h2>
+                    <p className="mt-0.5 text-sm font-medium text-gray-400">{job}</p>
+                </div>
+            </div>
+
+            <Button className="ml-auto rounded-full px-3 py-2 text-sm">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-5 w-5"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+                    />
+                </svg>
+            </Button>
+        </section>
     );
 }
-
 
 export default Profile;
